ed2ad0b26e1cf87920b5c6f33c4805ce2d747af6
Adam <Fireworksanimations21@gmail.com>
2024-06-29 23:47:24 +0200

    gekloot en niet overwonnen

--- InteractiveNarrative/Assets/_interactive/Scripts/VNCreator/0DynamicThoughts/Manager/ThoughtUIManager.cs
+++ InteractiveNarrative/Assets/_interactive/Scripts/VNCreator/0DynamicThoughts/Manager/ThoughtUIManager.cs
@@ -1,35 +1,83 @@
 using System.Collections;
-using System.Collections.Generic;
-using BlackBoard;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace VNCreator
+
+public class ThoughtUIManager : MonoBehaviour
 {
-    public class ThoughtUIManager : MonoBehaviour
+    public Slider slider;
+    public Canvas canvas;
+
+    public int downStep = 1; // Step size at which the slider value decreases
+    public float stepInterval = 0.5f; // Time interval between each step in seconds
+    private bool isGameOver = false;
+    private Coroutine countdownCoroutine;
+
+    private void Start()
     {
-        public Slider slider;
-        public Canvas canvas;
-        // Start is called before the first frame update
-        void Start()
+        slider.value = slider.maxValue; // Start the slider at the maximum value
+        EnableUI();
+    }
+
+    private void Update()
+    {
+        AddToSliderValue(1);
+    }
+
+    private IEnumerator DecreaseSliderValue()
+    {
+        while (!isGameOver)
         {
-            // EnableUI();
+            yield return new WaitForSeconds(stepInterval);
+
+            if (slider.value > slider.minValue)
+            {
+                slider.value -= downStep;
+
+                // Ensure the slider value does not drop below the minimum value
+                if (slider.value <= slider.minValue)
+                {
+                    slider.value = slider.minValue;
+                    isGameOver = true;
+                    Debug.Log("Game Over");
+                }
+            }
+
+            // Hide the handle if the value is at the minimum value
+            slider.handleRect.gameObject.SetActive(slider.value != slider.minValue);
+        }
+    }
+    public void EnableUI()
+    {
+        slider.value = slider.maxValue; // Start the slider at the maximum value
+        countdownCoroutine = StartCoroutine(DecreaseSliderValue());
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void AddToSliderValue(int valueToAdd)
+    {
+        // Stop the current countdown coroutine if active
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        // Add the value to the current slider value
+        slider.value = Mathf.Clamp(slider.value + valueToAdd, slider.minValue, slider.maxValue);
 
-        }
-
-        public void EnableUI()
+        // Restart the countdown if the game is not over
+        if (!isGameOver)
         {
-            canvas.gameObject.SetActive(true);
+            countdownCoroutine = StartCoroutine(DecreaseSliderValue());
         }
+    }
 
-        public void DisableUI()
+    private void OnDisable()
+    {
+        if (countdownCoroutine != null)
         {
-            canvas.gameObject.SetActive(false);
+            StopCoroutine(countdownCoroutine);
         }
+        isGameOver = false;
     }
 }
